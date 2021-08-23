1b88199bacac694b6b7cfe7697725081dabb1e8e
Floccari <nexy@tutanota.de>
2021-08-23 21:16:23 +0200

    saves scores between rounds

--- app/src/main/java/com/example/scopah/LeaderboardActivity.java
+++ app/src/main/java/com/example/scopah/LeaderboardActivity.java
@@ -3,7 +3,9 @@ package com.example.scopah;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.LinearLayoutCompat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +18,15 @@ public class LeaderboardActivity extends AppCompatActivity {
     private static final String NAMES_KEY = "NAMES";
     private static final String COLORS_KEY = "COLORS";
     private static final String SCORES_KEY = "SCORES";
+
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
+    private static final String LEADERBOARD_PREFERENCES_KEY = "com.example.scopah.SETTINGS_PREFERENCES_KEY";
+    private static final String SCORE_ONE_KEY = "SCORE_ONE";
+    private static final String SCORE_TWO_KEY = "SCORE_TWO";
+    private static final String SCORE_THREE_KEY = "SCORE_THREE";
+    private static final String SCORE_FOUR_KEY = "SCORE_FOUR";
 
     private ArrayList<String> names;
     private ArrayList<String> colors;
@@ -51,8 +62,13 @@ public class LeaderboardActivity extends AppCompatActivity {
 
         int size = names.size();
 
+        preferences = getSharedPreferences(LEADERBOARD_PREFERENCES_KEY, Context.MODE_PRIVATE);
+        editor = preferences.edit();
+
+
         if (intent.hasExtra("scores")) {
             scores = intent.getIntegerArrayListExtra("scores");
+            updateScores();
         } else {
             scores = new ArrayList<>();
 
@@ -60,6 +76,17 @@ public class LeaderboardActivity extends AppCompatActivity {
                 scores.add(new Integer(0));
         }
 
+        // write scores in SavedPreferences
+        editor.putInt(SCORE_ONE_KEY, scores.get(0).intValue());
+        editor.putInt(SCORE_TWO_KEY, scores.get(1).intValue());
+
+        if (size >= 3)
+            editor.putInt(SCORE_THREE_KEY, scores.get(2).intValue());
+
+        if (size == 4)
+            editor.putInt(SCORE_FOUR_KEY, scores.get(3).intValue());
+
+        editor.commit();
 
         // player one
         TextView name = (TextView) findViewById(R.id.text1);
@@ -106,6 +133,19 @@ public class LeaderboardActivity extends AppCompatActivity {
                 newGame();
             }
         });
+    }
+
+    private void updateScores() {
+        scores.set(0, scores.get(0) + preferences.getInt(SCORE_ONE_KEY, 0));
+        scores.set(1, scores.get(1) + preferences.getInt(SCORE_TWO_KEY, 0));
+
+        int size = scores.size();
+
+        if (size >= 3)
+            scores.set(2, scores.get(2) + preferences.getInt(SCORE_THREE_KEY, 0));
+
+        if (size == 4)
+            scores.set(3, scores.get(3) + preferences.getInt(SCORE_FOUR_KEY, 0));
     }
 
     private void newGame() {
